e3d799b5f49b5963a24b7d3ba8405abd75dc6414
ruzibekov <ruzibekov44@gmail.com>
2023-07-13 14:27:09 +0500

    add: details screen

--- app/src/main/java/com/ruzibekov/mystore/ui/screens/details/DetailsFragment.kt
+++ app/src/main/java/com/ruzibekov/mystore/ui/screens/details/DetailsFragment.kt
@@ -1,14 +1,12 @@
-package com.ruzibekov.mystore.ui.screens.home
+package com.ruzibekov.mystore.ui.screens.details
 
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import com.ruzibekov.mystore.R
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class DetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
     }
 }
