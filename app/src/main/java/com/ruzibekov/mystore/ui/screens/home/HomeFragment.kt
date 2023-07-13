95b9574697e63cde203a89e3a69645d362422225
ruzibekov <ruzibekov44@gmail.com>
2023-07-13 15:17:22 +0500

    init navigation component

--- app/src/main/java/com/ruzibekov/mystore/ui/screens/home/HomeFragment.kt
+++ app/src/main/java/com/ruzibekov/mystore/ui/screens/home/HomeFragment.kt
@@ -3,9 +3,12 @@ package com.ruzibekov.mystore.ui.screens.home
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.ruzibekov.mystore.R
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private var rvCategory: RecyclerView? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
